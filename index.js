5d41c369492636d37a9482ca564d2f62f9501425
narasaken <nazirasakenn@gmail.com>
2023-04-15 16:08:15 +0600

    added css and new elements thru js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// Create logo
+const logo = document.createElement("p");
+logo.innerText = "medium alike";
+document.body.appendChild(logo);
+logo.classList.add('logo');
+
+//Create header
+const header = document.createElement("h1");
+header.innerText = "Hello, world!";
+document.body.appendChild(header);
+header.classList.add('header');
+
 // async function getTopStories() {
 const getTopStories = async () => {
     const response = await fetch('https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?api-key=hc63TLTAw4QwqD4VEV4ceWl5Dr3fA3Uc')
